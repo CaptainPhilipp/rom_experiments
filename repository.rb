e3a15fd4398800b90d562982fcd70308efbe5612
CaptainPhilipp <Ph-s@mail.ru>
2017-08-07 13:25:58 +0300

    self-associated

--- repository.rb
+++ repository.rb
@@ -2,4 +2,15 @@ class CategoryRepo < ROM::Repository[:categories]
   relations :parameters
 
   commands :create
+
+  def with_parameters
+    # aggregate(:children_parameters).to_a
+    categories.combine(many: children_parameters)
+  end
+end
+
+class ParameterRepo < ROM::Repository[:parameters]
+  relations :categories
+
+  commands :create
 end
